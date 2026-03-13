c164a9d948c8c8cfb7b956e6296a2d66cefb25fe
Vatsa Kala <vatsakala121@gmail.com>
2026-03-13 02:03:02 -0500

    Question 38

--- SQL50/Question38.sql
+++ SQL50/Question38.sql
@@ -10,24 +10,25 @@ ORDER BY
 LIMIT
 */
 ---# Write your MySQL query statement below
-(
-    SELECT u.name AS results
-    FROM MovieRating mr
-    JOIN Users u ON mr.user_id = u.user_id
-    GROUP BY mr.user_id
-    ORDER BY COUNT(*) DESC, u.name
-    LIMIT 1
-)
+# Write your MySQL query statement below
+(SELECT u.name as results
+FROM Users u
+JOIN MovieRating mr
+ON u.user_id = mr.user_id
+GROUP BY u.user_id, u.name
+ORDER BY COUNT(rating)  DESC, u.name ASC
+LIMIT 1)
+
 UNION ALL
-(
-    SELECT m.title AS results
-    FROM MovieRating mr
-    JOIN Movies m ON mr.movie_id = m.movie_id
-    WHERE mr.created_at LIKE '2020-02%'
-    GROUP BY mr.movie_id
-    ORDER BY AVG(mr.rating) DESC, m.title
-    LIMIT 1
-);
+
+(select M.title as results
+FROM Movies M
+JOIN MovieRating mr
+ON M.movie_id = mr.movie_id
+WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY M.movie_id, M.title
+ORDER BY AVG(mr.rating) DESC, M.title ASC
+LIMIT 1);
 
 /*
 341. Movie Rating
