0c78aa4bcc2b4589664f9b69da1d31c4241618d1
Vatsa Kala <vatsakala121@gmail.com>
2026-03-19 23:56:44 -0500

    Question 47

---
+++ SQL50/Question47.sql
@@ -0,0 +1,69 @@
+--- # Write your MySQL query statement below
+With rank_table as (
+    SELECT salary, id, DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+    FROM Employee
+)
+
+SELECT MAX(CASE WHEN rnk = 2 THEN salary else null END) as SecondHighestSalary 
+FROM rank_table
+
+/*
+176. Second Highest Salary
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+SQL Schema
+Pandas Schema
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+*/
