c6e363a91c5930a148f77fedaeb7848521be5e1b
Vatsa Kala <vatsakala121@gmail.com>
2026-03-31 19:59:55 -0500

    Question 1070

---
+++ SQL50_&_Database/Question1070.sql
@@ -0,0 +1,67 @@
+--- 
+WITH min_year as (
+    SELECT product_id , MIN(year) as first_year
+    FROM Sales
+    Group by product_id
+)
+SELECT S.product_id, S.year as first_year, S.quantity, S.price
+FROM Sales S JOIN min_year M
+ON S.product_id = M.product_id
+AND S.year = M.first_year
+
+/*
+1070. Product Sales Analysis III
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+SQL Schema
+Pandas Schema
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+Each row records a sale of a product in a given year.
+A product may have multiple sales entries in the same year.
+Note that the per-unit price.
+
+Write a solution to find all sales that occurred in the first year each product was sold.
+
+For each product_id, identify the earliest year it appears in the Sales table.
+
+Return all sales entries for that product in that year.
+
+Return a table with the following columns: product_id, first_year, quantity, and price.
+Return the result in any order.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+*/
