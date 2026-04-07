0c8e80b7849f30f0e841e5f95ff16ec0224e3643
Vatsa Kala <vatsakala121@gmail.com>
2026-04-07 00:13:19 -0500

    Question1393

--- SQL50_&_Database/Question1393.sql
+++ SQL50_&_Database/Question1393.sql
@@ -16,7 +16,6 @@ FROM total_sell TS JOIN total_buy TB
 ON TS.stock_name = TB.stock_name
 
 -- Better Case when logic
-# Write your MySQL query statement below
 
 SELECT stock_name,SUM(CASE WHEN operation='Buy' THEN price *-1
 ELSE price END) AS capital_gain_loss
