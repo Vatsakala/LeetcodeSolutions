f56698e922a5e6d715438646ba92a4b9d2625d60
Vatsa Kala <vatsakala121@gmail.com>
2026-04-10 21:32:18 -0500

    Question 1667

---
+++ SQL50_&_Database/Question1667.sql
@@ -0,0 +1,51 @@
+-- Write YOUR SQL query statement here
+SELECT user_id,  CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) as name
+FROM Users
+-- CONCAT SUBSTRING TO BREAK STRINGS THEN UPPER LOWER TO CAPITAISE DECAPITALISE
+/*
+1667. Fix Names in a Table
+Attempted
+Easy
+Topics
+premium lock icon
+Companies
+SQL Schema
+Pandas Schema
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+*/
