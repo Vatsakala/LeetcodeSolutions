4674928449d92e06265e1025c0ec7168c5667f56
Vatsa Kala <vatsakala121@gmail.com>
2026-07-11 08:54:40 -0500

    Question 73

---
+++ DSA/set_matrix_zero.py
@@ -0,0 +1,31 @@
+from typing import List
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        ROWS ,COLS = len(matrix), len(matrix[0])
+        valuezero = False
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+
+                    if r>0:
+                        matrix[r][0] = 0
+                    else: 
+                        valuezero = True
+      
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+                
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+                
+        if valuezero:
+            for c in range(COLS):
+                matrix[0][c] = 0
