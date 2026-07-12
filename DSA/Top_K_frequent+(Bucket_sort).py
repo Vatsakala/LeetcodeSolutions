62d6949bc202ed981d4d2b0ac00d974d6b4eb50d
Vatsa Kala <vatsakala121@gmail.com>
2026-07-12 16:42:38 -0500

    Question 3

--- DSA/Top_K_frequent+(Bucket_sort).py
+++ DSA/Top_K_frequent+(Bucket_sort).py
@@ -1,6 +1,6 @@
 from typing import List
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums: List[int], k: int):
         count = {}
         freq = [[] for i in range(len(nums)+1)]
 
